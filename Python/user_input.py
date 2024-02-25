4ba9d26aac0eb5aee4d5d0490bdbe55e8da9633b
AdyBay <iyanuholuwahpo2000@gmail.com>
2024-02-25 07:15:49 +0100

    first commit

---
+++ Python/user_input.py
@@ -0,0 +1,7 @@
+#taking my first user input in Python.
+
+name = input('Enter your name: ')
+age = input('your age: ')
+location = input('your location: ')
+
+print(f"Hello,{name}, you are{age} years old and you live in {location}")
